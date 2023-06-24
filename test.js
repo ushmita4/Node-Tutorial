da0da3351d6cbf7399a22680e53277b88c571f1e
Ushmita Dutta <ushmitakhushi11092000@gmail.com>
2023-06-24 19:35:29 +0530

    Function Tutorial

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-const name='Ushmita'
-console.log(name);
+// const name='Ushmita'
+// console.log(name);
+
+const greet=(name)=>{
+    console.log(`hello, ${name}`);
+}
+greet('Ushmita');
+greet('Khushi');
